fc0f1c7c1dbac1d6a4ac8b76258dfbd8abf89b66
Aayush9029 <aayushpokharel36@gmail.com>
2021-10-21 20:18:02 -0400

    Initial Commit

---
+++ BetterWeather/BetterWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterWeatherApp.swift
+//  BetterWeather
+//
+//  Created by Aayush Pokharel on 2021-10-21.
+//
+
+import SwiftUI
+
+@main
+struct BetterWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterWeather
+//
+//  Created by Aayush Pokharel on 2021-10-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
